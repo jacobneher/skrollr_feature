4e2f25b57e6b5b491c75aedf0fab9e39b58ab373
Jacob Neher <services@threeshadowsmedia.com>
2013-07-17 13:44:19 -0600

    Changed view override template file that was throwing an error message if background data was not set for a node.

--- theme_files/views/views-view-unformatted--skrollr.tpl.php
+++ theme_files/views/views-view-unformatted--skrollr.tpl.php
@@ -10,7 +10,12 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <?php $bg_data = $view->result[$id]->_field_data['nid']['entity']->field_skrollr_background_data['und'][0]['value']; ?>
+  <?php
+    $bg_data = '';
+    if (!empty($view->result[$id]->_field_data['nid']['entity']->field_skrollr_background_data)) {
+      $bg_data = $view->result[$id]->_field_data['nid']['entity']->field_skrollr_background_data['und'][0]['value'];
+    }
+  ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } print ' ' . $bg_data; ?>>
     <?php print $row; ?>
   </div>
